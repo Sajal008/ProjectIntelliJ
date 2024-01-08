b9f1b7a4295d9e4d94a05b7fc036b6756358656f
Sajal008 <Khadgi.Sajal@student.greenriver.edu>
2024-01-07 17:43:34 -0800

    print Something added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Author: Sajal");
+
+        printSth("Hello again world!");
     }
+    public static void printSth( String message)
+    {
+        System.out.println(message);
+    }
+
+
 }
